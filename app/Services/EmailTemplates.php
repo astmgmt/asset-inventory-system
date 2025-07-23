f2135836c791b25ebc7f5522f561c68b1408bbe4
Ryan Oliver <2304032@ub.edu.ph>
2025-07-23 14:16:03 +0800

    Updated Review Request button in email

--- app/Services/EmailTemplates.php
+++ app/Services/EmailTemplates.php
@@ -90,7 +90,7 @@ class EmailTemplates
                         </table>
                         
                         <div style="text-align: center; margin-top: 30px;">
-                            <a href="http://asset-management.test/approve/requests" 
+                            <a href="https://assetinventorysystem.online/approve/requests" 
                                 style="display: inline-block; 
                                     background-color: #2563eb; 
                                     color: #ffffff; 
